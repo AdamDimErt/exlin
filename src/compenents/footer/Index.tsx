3924f4c417f3f07028514a4092bac5def21d445f
Adamdim <101868494+AdamDimErt@users.noreply.github.com>
2023-02-23 16:32:21 +0600

    add emails

--- src/compenents/footer/Index.tsx
+++ src/compenents/footer/Index.tsx
@@ -4,6 +4,7 @@ import { YMaps, Map, Placemark } from "react-yandex-maps";
 import Image from 'next/image';
 import facebookSvg from '@/assets/facebook-svgrepo-com.svg';
 import instagramSvg from '@/assets/instagram-svgrepo-com.svg';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 function Footer() {
     const mapData = {
@@ -32,6 +33,12 @@ function Footer() {
 
                     <div className={styles.telephone}>
                         <a href="tel:+77076302770">+7(707)-630-27-70</a>
+
+                    </div>
+                    <div className={styles.mail}>
+                        <h2>Почтовые адреса</h2>
+                        <a href="mailto:buh@zhsa.kz"><MailOutlineIcon/>buh@zhsa.kz</a>
+                        <a href="mailto:CorpZHSA2015@gmail.com<"><MailOutlineIcon/> CorpZHSA2015@gmail.com</a>
 
                     </div>
 
